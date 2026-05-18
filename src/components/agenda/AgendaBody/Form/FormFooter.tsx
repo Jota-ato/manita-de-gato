723188381144009684707c1437552d6fa89a2c12
Jota-ato <julioc-01@outlook.com>
2026-05-18 08:49:11 -0600

    refactor change Form footer design and loading.

--- src/components/agenda/AgendaBody/Form/FormFooter.tsx
+++ src/components/agenda/AgendaBody/Form/FormFooter.tsx
@@ -4,6 +4,7 @@ import {
     DialogFooter,
 } from "@/components/ui/dialog";
 import { Spinner } from "@/components/ui/spinner";
+import { cn } from "@/lib/utils";
 
 interface FormFooterProps {
     isSubmitting: boolean
@@ -11,14 +12,15 @@ interface FormFooterProps {
 
 export default function FormFooter({ isSubmitting }: FormFooterProps) {
     return (
-        <DialogFooter>
-            <div>
+        <DialogFooter className={cn("flex items-center gap-2", isSubmitting ? 'justify-between' : 'justify-end' )}>
+            {isSubmitting && <div className="flex items-center justify-center p-2">
+                <Spinner /></div>}
+            <div className="flex items-center gap-2">
                 <DialogClose asChild>
                     <Button variant={'outline'}>Cerrar</Button>
                 </DialogClose>
                 <Button type="submit">¡Agendar!</Button>
             </div>
-            {isSubmitting && <Spinner />}
         </DialogFooter>
     )
 }
