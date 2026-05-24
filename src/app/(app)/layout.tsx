211b3b819688b86199b444f7384bb8e31eb0aec1
Jota-ato <julioc-01@outlook.com>
2026-05-24 08:55:42 -0600

    fix: solve display error

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -6,7 +6,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
             <AppSidebar />
-            <SidebarTrigger className="sticky top-2 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 md:hidden" />
+
+            <SidebarTrigger className="fixed top-8 sm:top-6 z-100 flex items-center gap-4 border-b px-4 md:hidden" />
+
             <main className="w-full">
                 {children}
             </main>
