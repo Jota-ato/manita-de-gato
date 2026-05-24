71d2045673312d6be920ed29a8ef9da93d246e68
Jota-ato <julioc-01@outlook.com>
2026-05-24 08:45:50 -0600

    fix: solver server component error

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,15 +1,12 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
-import { useIsMobile } from "@/hooks/use-mobile";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-
-    const isMobile = useIsMobile();
 
     return (
         <SidebarProvider>
             <AppSidebar />
-            { isMobile && <SidebarTrigger /> }
+            <SidebarTrigger className="sticky top-2 z-10 flex h-14 items-center gap-4 border-b bg-background px-4 md:hidden" />
             <main className="w-full">
                 {children}
             </main>
