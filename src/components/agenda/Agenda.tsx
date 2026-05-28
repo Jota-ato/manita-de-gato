69b52c85a4c8ede37d7603d346eaa3ca3208cbc4
Jota-ato <julioc-01@outlook.com>
2026-05-28 12:14:45 -0600

    refactor: start refactor of agenda

--- src/components/agenda/Agenda.tsx
+++ src/components/agenda/Agenda.tsx
@@ -1,6 +1,13 @@
 import AgendaView from "@/components/agenda/AgendaView";
+import { getEventsFromDay } from "@/lib/agenda";
+import { TIMEZONE } from "@/lib/supabase/utils/helpers";
+import { TZDate } from "@date-fns/tz";
 
 export default async function Agenda() {
+
+    const today = new TZDate(new Date, TIMEZONE)
+    const events = getEventsFromDay(today);
+
     return (
         <AgendaView />
     )
--- src/lib/agenda/index.ts
+++ src/lib/agenda/index.ts
@@ -1,6 +1,10 @@
 'use server'
 import { getAppointments } from '@/lib/calendar/service';
 import { GoogleCalendarEventSchema, GoogleCalendarEvent } from "@/lib/calendar/schemas";
+import { TZDate } from '@date-fns/tz';
+import { createClient } from '../supabase/server';
+import { startOfDay } from 'date-fns';
+import { AppointmentSchema, Appointment } from '../supabase/schemas';
 
 interface getEventsProps {
     timeMin: Date
@@ -28,4 +32,25 @@ export async function getEvents({ timeMin, timeMax, maxResults }: getEventsProps
         console.error("Error fetching events directly on server:", error);
         return [];
     }
+}
+
+export async function getEventsFromDay(day: TZDate): Promise<Appointment[]> {
+
+    const supabase = await createClient();
+
+    const { data, error } = await supabase
+        .from("Appointments")
+        .select('*')
+        .gte('timeMin', startOfDay(day))
+        .in('status', ['approved', 'paid']);
+
+    if (error) { 
+        console.error('Error consiguiendo las citas', error.message);
+        return [];
+    }
+
+    return data.map(element => {
+        const result = AppointmentSchema.safeParse(element);
+        if (!result.error) return result.data;
+    }).filter((element): element is Appointment => typeof element === 'object');
 }
