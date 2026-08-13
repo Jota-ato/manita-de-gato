d47c28a3aa3b2aeedaf91b13d9c2d729e1bbc411
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-08-13 10:31:59 -0600

    feat: add real copyright and julio webpage

--- src/shared/components/public/ui/footer-copyright.tsx
+++ src/shared/components/public/ui/footer-copyright.tsx
@@ -1,9 +1,16 @@
 export function FooterCopyright() {
-    const currentYear = new Date().getFullYear();
-
-    return (
-        <div className="border-t border-muted mt-8 pt-8 flex flex-col md:flex-row justify-between items-center gap-4 text-xs text-foreground font-light">
-            <p>© {currentYear} Todos los derechos reservados</p>
-        </div>
-    )
+  return (
+    <div className="border-t border-muted mt-8 pt-8 flex flex-col md:flex-row justify-between items-center gap-4 text-xs text-foreground font-light">
+      <p>©2026 Todos los derechos reservados</p>
+      <a
+        href="https://www.julio-zavala.me/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-sm"
+      >
+        Página realizada por{" "}
+        <span className="font-bold text-primary">Julio Zavala</span>
+      </a>
+    </div>
+  );
 }
