d71ed6c18ae68356dd24d3f3298a886e76003122
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-08-13 11:26:17 -0600

    feat: translate business-controls

--- src/app/dashboard/users/page.tsx
+++ src/app/dashboard/users/page.tsx
@@ -10,39 +10,33 @@ import { Container } from "@/shared/components/ui/container";
 import { Separator } from "@/shared/components/ui/separator";
 import { redirect } from "next/navigation";
 
-const title = "Users"
+const title = "Usuarios";
 import { generateMetadataTitle } from "@/shared/utils/generateMetadata";
 import { Metadata } from "next";
 export const metadata: Metadata = {
-    title: generateMetadataTitle(title),
-    description: "List of all users in the system, including their roles and permissions. This page allows administrators to manage user accounts, view user details, and perform actions such as editing or deleting users.",
-}
+  title: generateMetadataTitle(title),
+  description:
+    "Lista de usuarios registrados en la aplicación. Administra y gestiona los usuarios, sus roles y permisos desde esta página.",
+};
 
 export default async function UsersPage() {
-    const { isAdmin, isOwner, session } = await requireAuth()
-    const users = await usersService.getAllUsers();
+  const { isAdmin, isOwner, session } = await requireAuth();
+  const users = await usersService.getAllUsers();
 
-    if (!session) redirect("/auth/sign-in")
-    if (!isAdmin && !isOwner) redirect("/not-authorized")
-    const { user } = session
+  if (!session) redirect("/auth/sign-in");
+  if (!isAdmin && !isOwner) redirect("/not-authorized");
+  const { user } = session;
 
-    return (
-        <section className="min-h-screen py-8 md:py-12">
-            <Container className="space-y-8">
-                <Heading>{title}</Heading>
-                <Separator />
+  return (
+    <section className="min-h-screen py-8 md:py-12">
+      <Container className="space-y-8">
+        <Heading>{title}</Heading>
+        <Separator />
 
-                <UsersTable
-                    currentUser={user as UserWithProvider}
-                    users={users}
-                />
-            </Container>
-            <DeleteDialog
-                currentUser={user as User}
-            />
-            <EditDialog
-                currentUser={user as User}
-            />
-        </section>
-    )
+        <UsersTable currentUser={user as UserWithProvider} users={users} />
+      </Container>
+      <DeleteDialog currentUser={user as User} />
+      <EditDialog currentUser={user as User} />
+    </section>
+  );
 }
